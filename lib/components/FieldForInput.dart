347daa73d42fd9f15501a77d33a31001eac75417
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-05-25 00:12:17 +0530

    customize the widget

--- lib/components/FieldForInput.dart
+++ lib/components/FieldForInput.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class FieldForInput extends StatelessWidget {
   final String text;
   final BoxDecoration? decoration;
+  final Widget icon;
   const FieldForInput({
     Key? key,
     required this.text,
     this.decoration,
+    required this.icon,
   }) : super(key: key);
 
   @override
@@ -14,11 +16,12 @@ class FieldForInput extends StatelessWidget {
     return Container(
       padding: EdgeInsets.all(8.0),
       decoration: decoration,
-      child: TextField(
+      child: TextFormField(
         decoration: InputDecoration(
             border: InputBorder.none,
             hintText: text,
-            hintStyle: TextStyle(color: Colors.grey[400])
+            hintStyle: TextStyle(color: Colors.grey[400]),
+            prefixIcon: icon,
         ),
       ),
     );
