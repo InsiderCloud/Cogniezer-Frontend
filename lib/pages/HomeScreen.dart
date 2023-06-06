93f028715c49c27f6de4043126d8a11d86357dc3
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-06-06 15:18:26 +0530

    bottom navigation bar

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,15 +1,48 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../constants.dart';
+
+
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cogniezer"),
-        backgroundColor: Colors.amberAccent,
-      )
-    );
+        backgroundColor: kPrimaryColorG1,
+
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
+        backgroundColor: Colors.white,
+        color: kPrimaryColorG1,
+        animationDuration: Duration(milliseconds: 300),
+        items: [
+          Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+        ],
+
+      ),
+    );;
   }
 }
+
