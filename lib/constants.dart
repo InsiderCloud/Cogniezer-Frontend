ec7c393ecafff70f459dbd4172b24c2281ede1c8
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-06-09 20:48:47 +0530

    add new color

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColorG1 = Color(0xFFB24592);
 const kPrimaryColorG2 = Color(0xFFF15F79);
+const HomeScreenBtnBgColor = Color(0xFFF58FA1);
 
 const double defaultPadding = 16.0;
