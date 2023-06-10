4ad22de846f7d405a4b4d3630c06ceec29b2075c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-06-10 20:20:39 +0530

    home content

--- lib/pages/HomeContent.dart
+++ lib/pages/HomeContent.dart
@@ -9,130 +9,136 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        color: kPrimaryColorG1,
-        height: 350,
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //Hi User
-                    Text(
-                      "Hi, Name!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    // Notification
-                    TextButton(
-                      onPressed: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: kPrimaryColorG2,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        padding: EdgeInsets.all(12),
-                        child: Icon(
-                          Icons.notifications,
+      body: ClipRRect(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+        child: Container(
+          color: kPrimaryColorG1,
+          height: 320,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //Hi User
+                      Text(
+                        "Hi, Name!",
+                        style: TextStyle(
                           color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "How do you feel today?",
-                      style: TextStyle(
+                      // Notification
+                      TextButton(
+                        onPressed: () {},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: HomeScreenBtnBgColor,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.all(12),
+                          child: Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "How do you feel today?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
                         color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
                       ),
-                    ),
-                    Icon(
-                      Icons.more_horiz,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
 
-                //Emojies
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        EmojiFace(
-                          emojiFace: '😊',
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          "Fine",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmojiFace(
-                          emojiFace: '😃',
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          "Excellent",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmojiFace(
-                          emojiFace: '😔',
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          "Sad",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmojiFace(
-                          emojiFace: '😠',
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          "Angry",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
+                  //Emojies
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          EmojiFace(
+                            emojiFace: '😊',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Fine",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmojiFace(
+                            emojiFace: '😃',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Excellent",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmojiFace(
+                            emojiFace: '😔',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Sad",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmojiFace(
+                            emojiFace: '😠',
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Angry",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
