f65efb95ccb165be3819cde1f41fb3213580ed02
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-06-11 20:01:00 +0530

    name , gmai and picture

--- lib/pages/SettingScreen.dart
+++ lib/pages/SettingScreen.dart
@@ -1,16 +1,51 @@
+import 'package:cogniezer_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
-  const SettingScreen ({Key? key}) : super(key: key);
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    body: Center(
-        child: Text("Setting Screen",
-            style: TextStyle(fontSize: 24),
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(12),
+          physics: BouncingScrollPhysics(),
+          children: [
+            Container(
+              height: 35,
+            ),
+            userProfile(),
+          ],
         ),
-    ),
+      ),
     );
   }
+}
+
+Widget userProfile() {
+  return Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: ListTile(
+      leading: CircleAvatar(
+        backgroundImage:
+            AssetImage("assets/images/ernest-flowers-P62INeB2yz4-unsplash.jpg"),
+      ),
+      title: Text(
+        "Sampath Chathuranga",
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 24, color: kPrimaryColorG1),
+      ),
+      subtitle: Text(
+        "abcdef@gmail.com",
+        style: TextStyle(fontSize: 16, color: kPrimaryColorG1),
+      ),
+    ),
+  );
 }
