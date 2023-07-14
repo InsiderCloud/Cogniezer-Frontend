79d93e391a599bf94dce05f8960a5cc28823678f
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-07-14 17:59:16 +0530

    todo item design

---
+++ lib/components/toDoItem.dart
@@ -0,0 +1,64 @@
+import 'package:cogniezer_app/constants.dart';
+import 'package:flutter/material.dart';
+
+import '../todo.dart';
+
+class toDoItem extends StatelessWidget {
+  final ToDo todo;
+  const toDoItem({Key? key, required this.todo}) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 20),
+          color: Colors.grey[200],
+          child: ListTile(
+            onTap: () {
+              print('Clicked on to do item');
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            tileColor: Colors.white,
+            leading: Icon(
+              todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+              color: Colors.blue,
+            ),
+            title: Text(
+              todo.todoText,
+              style: TextStyle(
+                fontSize: 16,
+                color: kPrimaryColorG1,
+                decoration: todo.isDone ? TextDecoration.lineThrough : null,
+              ),
+            ),
+            trailing: Container(
+              padding: EdgeInsets.all(0),
+              margin: EdgeInsets.symmetric(vertical: 12),
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                color: Colors.white,
+                iconSize: 18,
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  print("clicked delete button");
+                },
+
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
