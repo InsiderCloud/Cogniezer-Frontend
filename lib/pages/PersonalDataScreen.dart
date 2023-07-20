c2253866c1f1bb61dbd38678a70fb81b851d596f
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-07-20 13:14:34 +0530

    edit profile ui

--- lib/pages/PersonalDataScreen.dart
+++ lib/pages/PersonalDataScreen.dart
@@ -68,6 +68,53 @@ class PersonalDataScreen extends StatelessWidget {
                   topRight: Radius.circular(40.0),
                 ),
               ),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 15,
+                  top: 20,
+                  right: 15,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: ListView(
+                    children: [
+                      Center(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 130,
+                              height: 130,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 4,
+                                  color:Colors.white
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    spreadRadius: 2,
+                                    blurRadius: 10,
+                                    color: Colors.black.withOpacity(0.1)
+                                  )
+                                ],
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(
+                                      "assets/images/user.png"
+                                  ),
+                                )
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ],
