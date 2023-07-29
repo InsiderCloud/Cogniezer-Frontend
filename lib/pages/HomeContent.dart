0804b3074a9e1fa02c2dd2cdd1e2f6b5010242df
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-07-29 23:00:52 +0530

    activities section

--- lib/pages/HomeContent.dart
+++ lib/pages/HomeContent.dart
@@ -46,140 +46,262 @@ class _HomeContentState extends State<HomeContent> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: ClipRRect(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(25),
-          bottomRight: Radius.circular(25),
-        ),
-        child: Container(
-          color: kPrimaryColorG1,
-          height: 320,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+            child: Container(
+              color: kPrimaryColorG1,
+              height: 320,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
                     children: [
-                      //Hi User
-                      Text(
-                        "Hi, $_userName!",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      // Notification
-                      TextButton(
-                        onPressed: () {},
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: HomeScreenBtnBgColor,
-                            borderRadius: BorderRadius.circular(12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          //Hi User
+                          Text(
+                            "Hi, $_userName!",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          padding: EdgeInsets.all(12),
-                          child: Icon(
-                            Icons.notifications,
+                          // Notification
+                          TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: HomeScreenBtnBgColor,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              padding: EdgeInsets.all(12),
+                              child: Icon(
+                                Icons.notifications,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "How do you feel today?",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.more_horiz,
                             color: Colors.white,
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "How do you feel today?",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      SizedBox(
+                        height: 25,
                       ),
-                      Icon(
-                        Icons.more_horiz,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
 
-                  //Emojies
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
+                      //Emojies
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          EmojiFace(
-                            emojiFace: '😊',
+                          Column(
+                            children: [
+                              EmojiFace(
+                                emojiFace: '😊',
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Fine",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                            height: 8,
+                          Column(
+                            children: [
+                              EmojiFace(
+                                emojiFace: '😃',
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Excellent",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          Text(
-                            "Fine",
-                            style: TextStyle(color: Colors.white),
+                          Column(
+                            children: [
+                              EmojiFace(
+                                emojiFace: '😔',
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Sad",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          EmojiFace(
-                            emojiFace: '😃',
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            "Excellent",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          EmojiFace(
-                            emojiFace: '😔',
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            "Sad",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          EmojiFace(
-                            emojiFace: '😠',
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            "Angry",
-                            style: TextStyle(color: Colors.white),
+                          Column(
+                            children: [
+                              EmojiFace(
+                                emojiFace: '😠',
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "Angry",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                  "Activities for Relaxation",
+                style: TextStyle(
+                  color: kPrimaryColorG1,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Divider(
+            indent: 30.0,
+            endIndent: 30.0,
+            color: kPrimaryColorG1,
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    InkWell(
+                      child: activityCard(
+                          name: 'Music',
+                          context: context,
+                          img: 'assets/images/music.jpg'),
+                      onTap: () {},
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      child: activityCard(
+                          name: 'Meditation',
+                          context: context,
+                          img: 'assets/images/meditation.jpg'),
+                      onTap: () {},
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      child: activityCard(
+                          name: 'Breath Exercises',
+                          context: context,
+                          img: 'assets/images/breath exer.jpg'),
+                      onTap: () {},
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      child: activityCard(
+                          name: 'Yoga',
+                          context: context,
+                          img: 'assets/images/yoga.jpeg'),
+                      onTap: () {},
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+          )
+
+        ],
       ),
     );
   }
+
+  Widget activityCard({
+    required String name,
+    required BuildContext context,
+    required String img,
+  }) {
+    return Stack(
+      children: [
+        Container(
+          width: 380,
+          height: 150,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: kPrimaryColorG1,
+              width: 2,
+            ),
+          ),
+        ),
+
+        Positioned(
+          top: 50,
+          left: 10,
+          child: Text(
+            name,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: kPrimaryColorG1,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 20,
+          right: 10,
+          child: Image.asset(
+            img,
+            width: 120,
+            height: 120,
+          ),
+        ),
+      ],
+    );
+  }
+
 }
