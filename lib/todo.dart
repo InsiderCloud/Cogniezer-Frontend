fb8e48c37849b8a2312c59a1b8a2c846212cfb9a
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-07-29 23:02:06 +0530

    ToDo data changed

--- lib/todo.dart
+++ lib/todo.dart
@@ -11,12 +11,11 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Meet math lecture', isDone: true),
-      ToDo(id: '02', todoText: 'Chech mails', isDone: true),
-      ToDo(id: '03', todoText: 'Work on group project'),
-      ToDo(id: '04', todoText: 'Statistics Assignment'),
-      ToDo(id: '05', todoText: 'Designing a flyer'),
-      ToDo(id: '06', todoText: 'Shopping'),
+      ToDo(id: '01', todoText: 'Meet math lecture'),
+      ToDo(id: '02', todoText: 'Chech mails'),
+      ToDo(id: '03', todoText: 'Statistics Assignment', isDone: true),
+      ToDo(id: '04', todoText: 'Designing a flyer'),
+      ToDo(id: '05', todoText: 'Shopping',isDone: true),
 
 
     ];
