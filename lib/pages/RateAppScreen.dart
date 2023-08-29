c4ca22df864e3c93fe0d9a22f3caa932183c3a9e
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-08-29 19:54:02 +0530

    Rate App screen

--- lib/pages/RateAppScreen.dart
+++ lib/pages/RateAppScreen.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class RateAppScreen extends StatelessWidget {
   const RateAppScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: kPrimaryColorG1,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 20.0,
+            ),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  color: Colors.white,
+                  onPressed: () {
+                    // Handle back arrow button press here
+                    Navigator.pop(context);
+                  },
+                ),
+                const Expanded( // Wrap the text with Expanded to take available space
+                  child: Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.logout),
+                  color: Colors.white,
+                  onPressed: () {
+                  },
+                ),
+              ],
             ),
           ),
-        ),
-        body: Center(
-          child: Text("Rate Screen",
-            style: TextStyle(fontSize: 24),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40.0),
+                  topRight: Radius.circular(40.0),
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
