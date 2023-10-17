846b5f728821b9d63d75a72e2802ec954e67a7ce
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2023-10-17 22:59:34 +0530

    task reminder screen

---
+++ lib/pages/TaskReminderScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TaskReminderScreen extends StatelessWidget {
+  const TaskReminderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+            "Task Reminder"
+        ),
+      ),
+    );
+  }
+}
